dc65fed26b5eeb947af497b59da33392c65d0426
rherschke <github@sonstiges.herschke.de>
2013-05-21 21:37:32 +0200

    seperated the api and the technologies in seperate maven modules

---
+++ dynamic-repositories-extension/src/main/java/de/etecture/opensource/dynamicrepositories/extension/RepositoryExtension.java
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the ETECTURE Open Source Community Projects.
+ *
+ * Copyright (c) 2013 by:
+ *
+ * ETECTURE GmbH
+ * Darmstädter Landstraße 112
+ * 60598 Frankfurt
+ * Germany
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package de.etecture.opensource.dynamicrepositories.extension;
+
+import de.etecture.opensource.dynamicrepositories.api.Repository;
+import de.etecture.opensource.dynamicrepositories.spi.Technology;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.enterprise.inject.spi.ProcessInjectionTarget;
+
+/**
+ *
+ * @author rhk
+ */
+public class RepositoryExtension implements Extension {
+    private Set<Class<?>> repositoryInterfaces = new HashSet<>();
+    private Map<RepositoryKey, RepositoryBean> repositoryBeans = new HashMap<>();
+
+	void beforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd) {
+		System.out.println("beginning the scanning process");
+	}
+
+	<T> void processAnnotatedType(@Observes ProcessAnnotatedType<T> pat) {
+        if (pat.getAnnotatedType().isAnnotationPresent(Repository.class)) {
+            System.out.println("... found repository interface type: " + pat.getAnnotatedType().getJavaClass().getName());
+            repositoryInterfaces.add(pat.getAnnotatedType().getJavaClass());
+		}
+	}
+
+    @SuppressWarnings("element-type-mismatch")
+    <T> void processInjectionPoint(@Observes ProcessInjectionTarget<T> pit, BeanManager beanManager) {
+		for (InjectionPoint point : pit.getInjectionTarget().getInjectionPoints()) {
+            if (repositoryInterfaces.contains(point.getType())) {
+                System.out.printf("... found InjectionPoint in Class: %s with name: %s for the repository with type: %s%n", point.getBean().getBeanClass().getName(), point.getMember().getName(), point.getType());
+                String technology = "default";
+                if (point.getAnnotated().isAnnotationPresent(Technology.class)) {
+                    technology = point.getAnnotated().getAnnotation(Technology.class).value();
+                }
+                RepositoryKey key = new RepositoryKey((Class<?>) point.getType(), technology);
+                if (!repositoryBeans.containsKey(key)) {
+                    System.out.printf("... create Bean for Repository: %s with technology: %s%n", point.getType(), technology);
+                    repositoryBeans.put(key, new RepositoryBean(beanManager, key));
+                }
+            }
+		}
+	}
+
+	void afterBeanDiscovery(@Observes AfterBeanDiscovery abd) {
+		System.out.println("finished the scanning process");
+        for (RepositoryBean bean : repositoryBeans.values()) {
+			abd.addBean(bean);
+		}
+	}
+}
