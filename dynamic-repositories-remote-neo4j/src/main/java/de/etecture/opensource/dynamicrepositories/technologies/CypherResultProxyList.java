7444d2c1475ae68d16c785b7dfbc4e2c1b9fb901
rherschke <robert.herschke@etecture.de>
2013-05-31 11:00:03 +0200

    added the license headers and logic to return single value when a single value is requested, but the result is a list

--- dynamic-repositories-remote-neo4j/src/main/java/de/etecture/opensource/dynamicrepositories/technologies/CypherResultProxyList.java
+++ dynamic-repositories-remote-neo4j/src/main/java/de/etecture/opensource/dynamicrepositories/technologies/CypherResultProxyList.java
@@ -1,5 +1,45 @@
+/*
+ * This file is part of the ETECTURE Open Source Community Projects.
+ *
+ * Copyright (c) 2013 by:
+ *
+ * ETECTURE GmbH
+ * Darmstädter Landstraße 112
+ * 60598 Frankfurt
+ * Germany
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 package de.etecture.opensource.dynamicrepositories.technologies;
 
+import de.etecture.opensource.dynamicrepositories.technologies.utils.ResultHelper;
 import java.lang.reflect.Proxy;
 import java.util.AbstractList;
 import java.util.HashMap;
@@ -32,12 +72,12 @@ public class CypherResultProxyList<T> extends AbstractList<T> {
             return values.get(index);
         } else {
             if (result.containsKey(propertyName)) {
-                return (T) extractValue(result.get(propertyName), index);
+                return (T) ResultHelper.extractValue(result.get(propertyName), index);
             } else if (clazz.isInterface()) {
                 Map<String, Object> nestedResult = new HashMap<>();
                 for (Entry<String, Object> entry : result.entrySet()) {
                     if (entry.getKey().startsWith(propertyName + ".")) {
-                        nestedResult.put(entry.getKey().substring(propertyName.length() + 1), extractValue(entry.getValue(), index));
+                        nestedResult.put(entry.getKey().substring(propertyName.length() + 1), ResultHelper.extractValue(entry.getValue(), index));
                     }
                 }
                 T nestedProxy = (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, new CypherResultProxy("", nestedResult));
@@ -75,15 +115,4 @@ public class CypherResultProxyList<T> extends AbstractList<T> {
         return size;
     }
 
-    private Object extractValue(Object value, int index) {
-        if (value instanceof List) {
-            return ((List) value).get(index);
-        } else if (value instanceof Object[]) {
-            return ((Object[]) value)[index];
-        } else if (index == 0) {
-            return value;
-        } else {
-            throw new ArrayIndexOutOfBoundsException(index);
-        }
-    }
 }
